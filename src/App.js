4c81ee6c801d8eafa89ffb5799174d93af368ab3
Romina Bocon <63875657+rominabocon@users.noreply.github.com>
2022-05-27 19:22:33 -0300

    navbar
    este es mi navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <p>Hola mundo</p>
     </div>
   );
